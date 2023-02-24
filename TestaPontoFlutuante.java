948a0b6898f9d1bb3e76dfaf2069576f299cad69
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2023-02-24 10:03:29 -0300

    add: Variables Tests

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,14 @@
+public class TestaPontoFlutuante {
+  public static void main(String[] args){
+    double salario;
+    salario = 1000.54;
+
+    System.out.println("Meu salário é: " + salario);
+
+    int divisao = 5 / 2;
+    System.out.println(divisao);
+
+    double divisao2 = 5.0 / 2;
+    System.out.println(divisao2);
+  }  
+}
